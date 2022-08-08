3e2e9415e6c00a898ccbf1448bd45b36e87a3106
Simre1 <simre4775@gmail.com>
2022-08-08 13:35:45 +0200

    Store as class

--- src/Hex/Internal/Component/UnboxedSet.hs
+++ src/Hex/Internal/Component/UnboxedSet.hs
@@ -17,47 +17,15 @@ unboxedSet (MaxEntities entities) = do
 newtype UnboxableStore a = UnboxableStore (SU.SparseSetUnboxed a) 
 
 instance Unboxable a => StoreClass UnboxableStore a where
-  storeClassContains (UnboxableStore set) = storeContains' set
-  storeClassGet (UnboxableStore set) = storeGet' set
-  storeClassPut (UnboxableStore set) = storePut' set
-  storeClassDelete (UnboxableStore set) = storeDelete' set
-  storeClassFor (UnboxableStore set) = storeFor' set
-  storeClassMembers (UnboxableStore set) = storeMembers' set
+  storeClassContains (UnboxableStore set) entity = SU.contains set (coerce entity)
+  storeClassGet (UnboxableStore set) entity = SU.unsafeLookup set (coerce entity)
+  storeClassPut (UnboxableStore set) entity val = SU.insert set (coerce entity) val
+  storeClassDelete (UnboxableStore set) entity = SU.remove set (coerce entity)
+  storeClassFor (UnboxableStore set) f = SU.for set (coerce f)
+  storeClassMembers (UnboxableStore set) = SU.size set
   -- {-# INLINE storeClassContains #-}
   -- {-# INLINE storeClassGet #-}
   -- {-# INLINE storeClassPut #-}
   -- {-# INLINE storeClassDelete #-}
   -- {-# INLINE storeClassFor #-}
   -- {-# INLINE storeClassMembers #-}      
-
-
-
-
-storeContains' :: Unboxable component => SU.SparseSetUnboxed component -> Entity -> IO Bool
-storeContains' !set !entity = SU.contains set (coerce entity)
-{-# INLINE storeContains' #-}
-
-storeGet' :: Unboxable component => SU.SparseSetUnboxed component -> Entity -> IO component
-storeGet' !set !entity = SU.unsafeLookup set (coerce entity)
-{-# INLINE storeGet' #-}
-
-storePut' :: Unboxable a => SU.SparseSetUnboxed a -> Entity -> a -> IO ()
-storePut' !set !entity !val = SU.insert set (coerce entity) val
-{-# INLINE storePut' #-}
-
-storeDelete' :: Unboxable a => SU.SparseSetUnboxed a -> Entity -> IO ()
-storeDelete' !set !entity = SU.remove set (coerce entity)
-{-# INLINE storeDelete' #-}
-
-storeFor' :: (MonadIO m, Unboxable a) => SU.SparseSetUnboxed a -> (Entity -> m ()) -> m ()
-storeFor' !set !f = SU.for set (coerce f)
-{-# INLINE storeFor' #-}
-
-storeMembers' :: SU.SparseSetUnboxed a -> IO Int
-storeMembers' !set = SU.size set
-{-# INLINE storeMembers' #-}
-
--- type UnboxComponent :: (Type -> Constraint,Type -> Constraint, Type -> Constraint)
--- type Unbox3 = Unbox
--- type Unbox1 = V.Vector VU.Vector
--- type Unbox2 = VM.MVector VMU.MVector
